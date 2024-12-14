e54c0f57f6f27068f4714bc09a9f3548195db6ef
amit.erande <amit.erande@sourcedgroup.com>
2024-12-14 13:06:47 +0530

    feat: updated masked snapshot check lambda

--- functions/check_masked_snapshot/app.py
+++ functions/check_masked_snapshot/app.py
@@ -19,25 +19,26 @@ def lambda_handler(event, context):
     client = boto3.client("rds")
 
     try:
-        print("Generating masked DB snapshot")
+        print("Checking the status of masked DB snapshot")
         response = client.describe_db_snapshots(
             DBInstanceIdentifier=DBInstanceIdentifier,
             Filters=[
                 {
-                    'Name': 'db-snapshot-id',
-                    'Values': [
-                       event['MaskedDBSnapshotIdentifier'],
-                    ]
+                    "Name": "db-snapshot-id",
+                    "Values": [
+                        event["MaskedDBSnapshotIdentifier"],
+                    ],
                 },
             ],
         )
-        event["MaskedDBSnapshotIdentifierStatus"] = response["DBSnapshots"][0]["Status"]
+        event["MaskedSnapshotStatus"] = response["DBSnapshots"][0]["Status"]
         if response["DBSnapshots"][0]["Status"] == "failed":
-            event["Error"] = f"Error creating snapshot of masked database: {response["DBSnapshots"][0]["Status"]}"
-            event["MaskedSnapshotStatus"] = "failure"
+            event["Error"] = (
+                f'Error creating snapshot of masked database: {response["DBSnapshots"][0]["Status"]}'
+            )
         return event
     except Exception as e:
-        event["MaskedSnapshotStatus"] = "failure"
+        event["MaskedSnapshotStatus"] = "failed"
         event["Error"] = f"Error checking snapshot status of masked DB: {e}"
         print(f"Error creating snapshot of masked DB: {e}")
         return event
